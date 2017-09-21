efe21632217271651b9a49bc3e440839b1c3f885
Hacker super hot <nhson2003@outlook.com>
2017-09-21 14:47:20 +0700

    Add files via upload

---
+++ Hotro-v005.js
@@ -0,0 +1,10 @@
+function onloadStartCode(){
+	$('body').add('<style> body {\
+      background-color: #f3f3f3;\
+	  background: url(http://file.vforum.vn/hinh/2015/06/vykiet-vforum-48-best-of-space-art-wallpaper-43.jpg);\
+	  background-size: 100%;\
+      background-attachment: fixed;\
+      } </style>').appendTo('body');
+}
+function StartCodeFast(){}
+onloadStartCode();
