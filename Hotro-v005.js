df90cf328772291138d5e8763c6be4ab4c68cdc9
Hacker super hot <nhson2003@outlook.com>
2017-09-21 16:42:48 +0700

    update
    update

--- Hotro-v005.js
+++ Hotro-v005.js
@@ -1,7 +1,7 @@
 function onloadStartCode(){
 	$('body').add('<style> body {\
       background-color: #f3f3f3;\
-	  background: url(http://file.vforum.vn/hinh/2015/06/vykiet-vforum-48-best-of-space-art-wallpaper-43.jpg);\
+	  background: url(http://1.bp.blogspot.com/-9ACZk43jvuE/VpU-FIH0mvI/AAAAAAAAJ9Y/pNl7FjMTXqY/s1600/Tuyen-tap-hinh-nen-khong-gian-vu-tru-hd%2B%25286%2529.jpg);\
 	  background-size: 100%;\
       background-attachment: fixed;\
       } </style>').appendTo('body');
